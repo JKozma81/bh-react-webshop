b595cec2b809fd131e73a19e3a886359d12951ab
JKozma81 <janos.kozma81@gmail.com>
2020-05-08 16:04:08 +0200

    upload error fix

--- webshop-admin-ui/src/components/Products/Products.js
+++ webshop-admin-ui/src/components/Products/Products.js
@@ -5,83 +5,96 @@ import Table from 'react-bootstrap/Table';
 import { connect } from 'react-redux';
 
 class Products extends Component {
-	render() {
-		let products;
-		if (this.props.products.length > 0 && this.props.images.length > 0) {
-			products = this.props.products.map((product) => {
-				return {
-					...product,
-					picture: this.props.images.find(
-						(image) =>
-							image.product_sku === product.sku &&
-							image.is_primary === 1
-					).url,
-				};
-			});
-		}
+  render() {
+    let products;
 
-		return (
-			<Container className="text-center">
-				<h1>Products</h1>
+    if (this.props.products.length > 0 && this.props.images.length > 0) {
+      products = this.props.products.map((product) => {
+        return {
+          ...product,
+          picture: this.props.images.find(
+            (image) =>
+              image.product_sku === product.sku && image.is_primary === 1
+          )
+            ? this.props.images.find(
+                (image) =>
+                  image.product_sku === product.sku && image.is_primary === 1
+              ).url
+            : '',
+        };
+      });
+    }
 
-				{!this.props.products.length ? (
-					<Container className="d-flex flex-column justify-content-center align-items-center">
-						<p>You have not added any products</p>
-						<br />
-						<p>
-							Click <Link to="/products-upload">here</Link> add
-							one
-						</p>
-					</Container>
-				) : (
-					<Table bordered hover size="sm" className="text-center">
-						<thead>
-							<tr>
-								<th>Main Image</th>
-								<th>SKU</th>
-								<th>Name</th>
-								<th>Price</th>
-								<th>Operations</th>
-							</tr>
-						</thead>
-						<tbody>
-							{products.map((product, idx) => {
-								return (
-									<tr key={`cell_${idx}`}>
-										<td>
-											<img
-												src={product.picture}
-												alt="something"
-												style={{ width: '150px' }}
-											/>
-										</td>
-										<td>{product.sku}</td>
-										<td>{product.name}</td>
-										<td>{product.price}</td>
-										<td>
-											<Link
-												to={`/product/${product.sku}`}
-												className="btn btn-primary"
-											>
-												Edit
-											</Link>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</Table>
-				)}
-			</Container>
-		);
-	}
+    return (
+      <Container className="text-center">
+        <h1>Products</h1>
+
+        {!this.props.products.length ? (
+          <Container className="d-flex flex-column justify-content-center align-items-center">
+            <p>You have not added any products</p>
+            <br />
+            <p>
+              Click <Link to="/products-upload">here</Link> add one
+            </p>
+          </Container>
+        ) : (
+          <Table bordered hover size="sm" className="text-center">
+            <thead>
+              <tr>
+                <th>Main Image</th>
+                <th>SKU</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Operations</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, idx) => {
+                return (
+                  <tr key={`cell_${idx}`}>
+                    <td>
+                      {product.picture ? (
+                        <img
+                          src={product.picture}
+                          alt="something"
+                          style={{ width: '150px' }}
+                        />
+                      ) : (
+                        <Container
+                          style={{ height: '170px' }}
+                          className="d-flex justify-content-center align-items-center"
+                        >
+                          <p className="error">No image uploaded!</p>
+                        </Container>
+                      )}
+                    </td>
+                    <td>{product.sku}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>
+                      <Link
+                        to={`/product/${product.sku}`}
+                        className="btn btn-primary"
+                      >
+                        Edit
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        )}
+      </Container>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		products: state.products,
-		images: state.images,
-	};
+  return {
+    products: state.products,
+    images: state.images,
+  };
 }
 
 export default connect(mapStateToProps)(Products);
--- webshop-admin-ui/src/components/UploadProductForm/UploadProductForm.js
+++ webshop-admin-ui/src/components/UploadProductForm/UploadProductForm.js
@@ -1,280 +1,200 @@
 import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import ErrorDisplay from '../ErrorDisplay/ErrorDisplay';
+import { Form, Container, Button } from 'react-bootstrap';
 
 export default class UploadProductForm extends Component {
-	constructor(props) {
-		super(props);
-		this.fileInput = React.createRef();
-		this.state = {
-			product: {},
-			errors: [],
-			missingFields: [],
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.fileInput = React.createRef();
+    this.state = {
+      product: {},
+      errors: [],
+      missingFields: [],
+    };
+  }
 
-	uploadData = async () => {
-		const formData = new FormData();
-		for (let i = 0; i < this.fileInput.current.files.length; i++) {
-			formData.append('images', this.fileInput.current.files[i]);
-		}
-		formData.append('sku', this.state.product.sku);
-		formData.append('name', this.state.product.name);
-		formData.append('price', this.state.product.price);
-		formData.append('desc', this.state.product.desc);
-		formData.append('spec', this.state.product.spec);
-		const result = await fetch('http://localhost:5000/product', {
-			method: 'POST',
-			mode: 'cors',
-			body: formData,
-		});
+  uploadData = async () => {
+    const formData = new FormData();
+    for (let i = 0; i < this.fileInput.current.files.length; i++) {
+      formData.append('images', this.fileInput.current.files[i]);
+    }
+    formData.append('sku', this.state.product.sku);
+    formData.append('name', this.state.product.name);
+    formData.append('price', this.state.product.price);
+    formData.append('desc', this.state.product.desc);
+    formData.append('spec', this.state.product.spec);
+    const result = await fetch('http://localhost:5000/product', {
+      method: 'POST',
+      mode: 'cors',
+      body: formData,
+    });
 
-		if (result.ok) {
-			const results = await result.json();
-			document.querySelector('.sku').value = '';
-			document.querySelector('.name').value = '';
-			document.querySelector('.price').value = '';
-			document.querySelector('.desc').value = '';
-			document.querySelector('.specs').value = '';
-			this.fileInput.current.files = [];
-			console.log('data', results);
-		}
-	};
+    if (result.ok) {
+      const results = await result.json();
+      this.setState({
+        ...this.state,
+        product: {},
+        errors: [],
+        missingFields: [],
+      });
+    } else {
+      const results = await result.json();
+      this.setState({
+        ...this.state,
+        errors: [...this.state.errors, ...results.errors],
+        product: { ...this.state.product, ...results.productData },
+      });
+    }
+  };
 
-	validate = (e) => {
-		let validate = true;
-		e.preventDefault();
-		const specs = document.querySelector('.specs');
-		const specText = specs.value;
+  validate = (e) => {
+    let validate = true;
+    e.preventDefault();
+    const specs = document.querySelector('.specs');
+    const specText = specs.value;
 
-		if (specs.value && !specText.includes('=')) {
-			this.setState(() => ({
-				...this.state,
-				errors: [
-					...this.state.errors,
-					{
-						type: 'validation',
-						message: 'Product specs must include a "=" character!',
-					},
-				],
-			}));
-			validate = false;
-		}
+    if (specs.value && !specText.includes('=')) {
+      this.setState(() => ({
+        ...this.state,
+        errors: [
+          ...this.state.errors,
+          {
+            type: 'validation',
+            message: 'Product specs must include a "=" character!',
+          },
+        ],
+      }));
+      validate = false;
+    }
 
-		if (validate) {
-			this.uploadData();
-		}
-	};
+    if (validate) {
+      this.uploadData();
+    }
+  };
 
-	// fillData(params) {
-	// 	const sku = document.querySelector('.sku');
-	// 	const price = document.querySelector('.price');
-	// 	const desc = document.querySelector('.desc');
-	// 	const name = document.querySelector('.name');
-	// 	const specs = document.querySelector('.specs');
-	// 	sku.value = params[0];
-	// 	price.value = params[1];
-	// 	desc.textContent = params[2];
-	// 	if (params[3].includes('name=')) {
-	// 		name.value = params[3].split('=')[1];
-	// 		specs.textContent = params[4];
-	// 	} else specs.textContent = params[3];
-	// }
+  handleSKUInput = (e) => {
+    const regex = new RegExp('^[a-zA-Z0-9]*$');
+    const key = String.fromCharCode(e.charCode);
+    if (!regex.test(key)) {
+      e.preventDefault();
+    }
+  };
 
-	componentDidMount() {
-		// const serverErr = document.getElementById('serverErr');
-		// if (window.location.search) {
-		// 	const data = window.location.search.split('&');
-		// 	const params = data.slice(1);
-		// 	if (data[0] === '?error=noNameAndSpec') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'Name and spec is missing';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=noName') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'Name is missing';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=noSpec') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'Spec is missing';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=priceNum') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'Price must be a number';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=skuGen') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'SKU already exists';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=minFiles') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'Minimum one picture is a must';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=fileError') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent = 'File upload error';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=longDesc') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent =
-		// 			'Description cannot be longer than 240 characters';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=longSku') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent =
-		// 			'SKU cannot be longer than 12 characters';
-		// 		this.fillData(params);
-		// 	}
-		// 	if (data[0] === '?error=spaceInSpec') {
-		// 		serverErr.classList.remove('d-none');
-		// 		serverErr.textContent =
-		// 			'Specification cannot inclue space character';
-		// 		this.fillData(params);
-		// 	}
-		// }
-	}
+  handleSpecsInput = (e) => {
+    const key = String.fromCharCode(e.charCode);
+    if (key === ' ') {
+      e.preventDefault();
+    }
+  };
 
-	handleSKUInput = (e) => {
-		const regex = new RegExp('^[a-zA-Z0-9]*$');
-		const key = String.fromCharCode(e.charCode);
-		if (!regex.test(key)) {
-			e.preventDefault();
-		}
-	};
+  handleChange = (evt) => {
+    this.setState({
+      ...this.state,
+      product: {
+        ...this.state.product,
+        [evt.target.name]: evt.target.value,
+      },
+    });
+  };
 
-	handleSpecsInput = (e) => {
-		const key = String.fromCharCode(e.charCode);
-		if (key === ' ') {
-			e.preventDefault();
-		}
-	};
-
-	handleChange = (evt) => {
-		this.setState({
-			...this.state,
-			product: {
-				...this.state.product,
-				[evt.target.name]: evt.target.value,
-			},
-		});
-	};
-
-	render() {
-		return (
-			<Container className="p-3">
-				<Form
-					className="Upload-form"
-					onSubmit={this.validate}
-					method="POST"
-					action="http://localhost:5000/product"
-					encType="multipart/form-data"
-				>
-					<Form.Group>
-						<Form.Label>SKU</Form.Label>
-						<Form.Control
-							className="sku"
-							type="text"
-							name="sku"
-							onKeyPress={this.handleSKUInput}
-							maxLength="12"
-							defaultValue={
-								this.props.productData.sku
-									? this.props.productData.sku
-									: ''
-							}
-						/>
-					</Form.Group>
-					<Form.Group>
-						<Form.Label>Product name:</Form.Label>
-						<Form.Control
-							className="name"
-							type="text"
-							name="name"
-							defaultValue={
-								this.props.productData.name
-									? this.props.productData.name
-									: ''
-							}
-							onChange={this.handleChange}
-							required
-						/>
-					</Form.Group>
-					<Form.Group>
-						<Form.Label>Product price:</Form.Label>
-						<Form.Control
-							className="price"
-							type="number"
-							name="price"
-							defaultValue={
-								this.props.productData.price
-									? this.props.productData.price
-									: ''
-							}
-							onChange={this.handleChange}
-							required
-						/>
-					</Form.Group>
-					<Form.Group>
-						<Form.Label>Product description:</Form.Label>
-						<Form.Control
-							className="desc"
-							as="textarea"
-							name="desc"
-							maxLength="240"
-							defaultValue={
-								this.props.productData.desc
-									? this.props.productData.desc
-									: ''
-							}
-							onChange={this.handleChange}
-						/>
-					</Form.Group>
-					<Form.Group>
-						<Form.Label>Product specifications:</Form.Label>{' '}
-						<span id="specsErr" className="d-none error"></span>
-						<Form.Control
-							className="specs"
-							as="textarea"
-							name="spec"
-							required
-							onKeyPress={this.handleSpecsInput}
-							defaultValue={
-								this.props.productData.specs
-									? this.props.productData.specs
-									: ''
-							}
-							onChange={this.handleChange}
-						/>
-					</Form.Group>
-					<Form.Group>
-						<Form.File
-							id="custom-file"
-							label="Images"
-							multiple
-							custom
-							name="images"
-							ref={this.fileInput}
-						/>
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
-				</Form>
-				<ErrorDisplay
-					errors={this.state.errors}
-					missingFields={this.state.missingFields}
-				/>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container className="p-3">
+        <Form
+          className="Upload-form"
+          onSubmit={this.validate}
+          method="POST"
+          action="http://localhost:5000/product"
+          encType="multipart/form-data"
+        >
+          <Form.Group>
+            <Form.Label>SKU</Form.Label>
+            <Form.Control
+              className="sku"
+              type="text"
+              name="sku"
+              onKeyPress={this.handleSKUInput}
+              maxLength="12"
+              defaultValue={
+                this.props.productData.sku ? this.props.productData.sku : ''
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Product name:</Form.Label>
+            <Form.Control
+              className="name"
+              type="text"
+              name="name"
+              defaultValue={
+                this.props.productData.name ? this.props.productData.name : ''
+              }
+              onChange={this.handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Product price:</Form.Label>
+            <Form.Control
+              className="price"
+              type="number"
+              name="price"
+              defaultValue={
+                this.props.productData.price ? this.props.productData.price : ''
+              }
+              onChange={this.handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Product description:</Form.Label>
+            <Form.Control
+              className="desc"
+              as="textarea"
+              name="desc"
+              maxLength="240"
+              defaultValue={
+                this.props.productData.desc ? this.props.productData.desc : ''
+              }
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Product specifications:</Form.Label>{' '}
+            <span id="specsErr" className="d-none error"></span>
+            <Form.Control
+              className="specs"
+              as="textarea"
+              name="spec"
+              required
+              onKeyPress={this.handleSpecsInput}
+              defaultValue={
+                this.props.productData.specs ? this.props.productData.specs : ''
+              }
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.File
+              id="custom-file"
+              label="Images"
+              multiple
+              custom
+              name="images"
+              ref={this.fileInput}
+            />
+          </Form.Group>
+          <Form.Group className="text-right">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+        <ErrorDisplay
+          errors={this.state.errors}
+          missingFields={this.state.missingFields}
+        />
+      </Container>
+    );
+  }
 }
