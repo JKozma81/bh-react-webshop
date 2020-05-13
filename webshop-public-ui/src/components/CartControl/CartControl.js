d38e9e4624816924bfbb16a7d5f20600459b9ca2
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-05-13 11:01:37 +0200

    cart functionality with product card buttons working

--- webshop-public-ui/src/components/CartControl/CartControl.js
+++ webshop-public-ui/src/components/CartControl/CartControl.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { addToCart } from '../../actions/Actions';
+import { addToCart, removeOne } from '../../actions/Actions';
 
 class CartControl extends Component {
-  render() {
-    return (
-      <Container>
-        {this.props.cart.find(
-          (item) => item.sku === this.props.product.sku
-        ) && <Button>Remove one</Button>}
-        {this.props.product.qty}
-        {this.props.product.qty ? (
-          <Button onClick={() => this.props.addToCart(this.props.product.sku)}>
-            Add To Cart
-          </Button>
-        ) : (
-          <span>Out of stock</span>
-        )}
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				{this.props.cart.find(
+					(item) => item.sku === this.props.product.sku
+				) && (
+					<Button
+						onClick={() =>
+							this.props.removeOneFromCart(this.props.product.sku)
+						}
+					>
+						Remove one
+					</Button>
+				)}
+				{this.props.product.qty}
+				{this.props.product.qty ? (
+					<Button
+						onClick={() =>
+							this.props.addToCart(this.props.product.sku)
+						}
+					>
+						Add To Cart
+					</Button>
+				) : (
+					<span>Out of stock</span>
+				)}
+			</Container>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    cart: state.cart,
-  };
+	return {
+		cart: state.cart,
+	};
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    addToCart: (itemSku) => dispatch(addToCart(itemSku)),
-    // removeFromCart: (itemSku) => dispatch(),
-  };
+	return {
+		addToCart: (itemSku) => dispatch(addToCart(itemSku)),
+		removeOneFromCart: (itemSku) => dispatch(removeOne(itemSku)),
+	};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartControl);
--- webshop-public-ui/src/reducers/Rootreducer.js
+++ webshop-public-ui/src/reducers/Rootreducer.js
@@ -2,57 +2,124 @@ import InitialState from '../store/Store';
 import { ADD_TO_CART, REMOVE_ONE } from '../actions/Actions';
 
 export default function RootReducer(state = InitialState, action) {
-  switch (action.type) {
-    case ADD_TO_CART:
-      const cartItem = state.cart.find((item) => item.sku === action.itemSku);
-      const productInStock = state.products.find(
-        (item) => item.sku === action.itemSku
-      );
+	switch (action.type) {
+		case ADD_TO_CART: {
+			const cartItem = state.cart.find(
+				(item) => item.sku === action.itemSku
+			);
+			const productInStock = state.products.find(
+				(item) => item.sku === action.itemSku
+			);
+			let newCart;
 
-      return {
-        ...state,
-        products: state.products.map((product) => {
-          if (product.sku !== action.itemSku) {
-            return {
-              ...product,
-            };
-          }
+			if (cartItem) {
+				newCart = state.cart.map((itemInCart) => {
+					if (itemInCart.sku === cartItem.sku) {
+						return {
+							...itemInCart,
+							qty: itemInCart.qty + 1,
+						};
+					}
+					return {
+						...itemInCart,
+					};
+				});
+			} else {
+				newCart = [
+					...state.cart,
+					{
+						sku: productInStock.sku,
+						name: productInStock.name,
+						qty: 1,
+						price: productInStock.price,
+					},
+				];
+			}
 
-          return {
-            ...product,
-            qty: product.qty - 1 >= 0 ? product.qty - 1 : product.qty,
-          };
-        }),
+			const newProducts = state.products.map((product) => {
+				if (product.sku !== action.itemSku) {
+					return {
+						...product,
+					};
+				}
 
-        cart: cartItem
-          ? [
-              ...state.cart.map((itemInCart) => {
-                if (itemInCart.sku === cartItem.sku) {
-                  return {
-                    ...itemInCart,
-                    qty: itemInCart.qty + 1,
-                  };
-                }
-                return {
-                  ...itemInCart,
-                };
-              }),
-            ]
-          : [
-              ...state.cart,
-              {
-                sku: productInStock.sku,
-                name: productInStock.name,
-                qty: 1,
-                price: productInStock.price,
-              },
-            ],
-      };
+				return {
+					...product,
+					qty: product.qty - 1 >= 0 ? product.qty - 1 : product.qty,
+				};
+			});
 
-    case REMOVE_ONE:
-      return {};
+			return {
+				...state,
+				products: newProducts,
+				cart: newCart,
+			};
+		}
 
-    default:
-      return state;
-  }
+		case REMOVE_ONE: {
+			let cartItem = state.cart.find(
+				(item) => item.sku === action.itemSku
+			);
+			const productInStock = state.products.find(
+				(item) => item.sku === action.itemSku
+			);
+
+			let newCart;
+
+			const newProducts = state.products.map((product) => {
+				if (product.sku !== action.itemSku) {
+					return {
+						...product,
+					};
+				}
+
+				return {
+					...product,
+					qty: product.qty + 1,
+				};
+			});
+
+			if (cartItem) {
+				if (cartItem.qty - 1 <= 0) {
+					newCart = state.cart.filter((item) => {
+						if (item.sku !== cartItem.sku) {
+							return {
+								...item,
+							};
+						}
+					});
+				} else {
+					newCart = state.cart.map((itemInCart) => {
+						if (itemInCart.sku === cartItem.sku) {
+							return {
+								...itemInCart,
+								qty: itemInCart.qty - 1,
+							};
+						}
+						return {
+							...itemInCart,
+						};
+					});
+				}
+			} else {
+				newCart = [
+					...state.cart,
+					{
+						sku: productInStock.sku,
+						name: productInStock.name,
+						qty: 1,
+						price: productInStock.price,
+					},
+				];
+			}
+			return {
+				...state,
+				products: newProducts,
+				cart: newCart,
+			};
+		}
+
+		default:
+			return state;
+	}
 }
