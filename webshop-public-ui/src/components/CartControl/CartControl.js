3cb9d0defc96c47915203293785aab05bc049e91
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-05-15 16:51:30 +0200

    carousels missing and some frontend work needed to finish

--- webshop-public-ui/src/components/CartControl/CartControl.js
+++ webshop-public-ui/src/components/CartControl/CartControl.js
@@ -4,54 +4,58 @@ import { connect } from 'react-redux';
 import { addToCart, removeOne } from '../../actions/Actions';
 
 class CartControl extends Component {
-  render() {
-    return (
-      <>
-        <Col xs={5} className="p-0">
-          {this.props.cart.find(
-            (item) => item.sku === this.props.product.sku
-          ) && (
-            <Button
-              className="btn-sm"
-              onClick={() =>
-                this.props.removeOneFromCart(this.props.product.sku)
-              }
-            >
-              Remove one
-            </Button>
-          )}
-        </Col>
-        <Col xs={2} className="p-0 text-center">
-          {this.props.product.qty}
-        </Col>
-        <Col xs={5} className="p-0">
-          {this.props.product.qty ? (
-            <Button
-              className="btn-sm"
-              onClick={() => this.props.addToCart(this.props.product.sku)}
-            >
-              Add To Cart
-            </Button>
-          ) : (
-            <span>Out of stock</span>
-          )}
-        </Col>
-      </>
-    );
-  }
+	render() {
+		return (
+			<>
+				<Col xs={5} className="p-0">
+					{this.props.cart.find(
+						(item) => item.sku === this.props.product.sku
+					) && (
+						<Button
+							className="btn-sm"
+							onClick={() =>
+								this.props.removeOneFromCart(
+									this.props.product.sku
+								)
+							}
+						>
+							Remove one
+						</Button>
+					)}
+				</Col>
+				<Col xs={2} className="p-0 text-center">
+					{this.props.product.qty}
+				</Col>
+				<Col xs={5} className="p-0 text-center">
+					{this.props.product.qty ? (
+						<Button
+							className="btn-sm"
+							onClick={() =>
+								this.props.addToCart(this.props.product.sku)
+							}
+						>
+							Add To Cart
+						</Button>
+					) : (
+						<span>Out of stock</span>
+					)}
+				</Col>
+			</>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    cart: state.cart,
-  };
+	return {
+		cart: state.cart,
+	};
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    addToCart: (itemSku) => dispatch(addToCart(itemSku)),
-    removeOneFromCart: (itemSku) => dispatch(removeOne(itemSku)),
-  };
+	return {
+		addToCart: (itemSku) => dispatch(addToCart(itemSku)),
+		removeOneFromCart: (itemSku) => dispatch(removeOne(itemSku)),
+	};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartControl);
--- webshop-public-ui/src/components/Products/Products.js
+++ webshop-public-ui/src/components/Products/Products.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
-import { Container, Col, CardDeck } from 'react-bootstrap';
+import { Container, Col, CardDeck, Row } from 'react-bootstrap';
 import classes from './Products.module.css';
 import { connect } from 'react-redux';
 
 import ProductBox from '../ProductBox/ProductBox';
 
 class Products extends Component {
-  async componentDidMount() {}
+	async componentDidMount() {}
 
-  render() {
-    return (
-      <Container className={`${classes.Products} mt-5`}>
-        <CardDeck>
-          {this.props.products.map((product, idx) => (
-            <Col xs={3} key={`prod_${idx}`}>
-              <ProductBox product={product} />
-            </Col>
-          ))}
-        </CardDeck>
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container fluid className={`${classes.Products} p-5`}>
+				<Row xs={5} className="h-75">
+					{this.props.products.map((product, idx) => (
+						<Col key={`prod_${idx}`} className="p-3">
+							<ProductBox product={product} />
+						</Col>
+					))}
+				</Row>
+			</Container>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    products: state.products.map((item) => {
-      const primaryPicture = state.images.find(
-        (picture) =>
-          picture.product_sku === item.sku && picture.is_primary === 1
-      );
-      return {
-        ...item,
-        picture: primaryPicture,
-      };
-    }),
-  };
+	return {
+		products: state.products.map((item) => {
+			const primaryPicture = state.images.find(
+				(picture) =>
+					picture.product_sku === item.sku && picture.is_primary === 1
+			);
+			return {
+				...item,
+				picture: primaryPicture,
+			};
+		}),
+	};
 }
 
 export default connect(mapStateToProps)(Products);
